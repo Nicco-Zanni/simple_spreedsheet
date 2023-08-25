88d8dbfe59429b2417cd5e1ec34e8ad375165801
nicco <niccozanni@gmail.com>
2023-08-25 17:40:47 +0200

    Main was implemented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "Spreadsheet.h"
+
+class MyApp: public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    Spreadsheet *frame = new Spreadsheet(5,NULL, wxID_ANY, "Spreadsheet");
+    frame->Show( true );
+    return true;
 }
