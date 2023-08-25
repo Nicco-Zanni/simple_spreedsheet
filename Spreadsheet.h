f7c2c72ddab30b8312edc6549ae9d6524204f115
nicco <niccozanni@gmail.com>
2023-08-25 16:59:30 +0200

    Private attributes and constructor were added in Spreadsheet.h

--- Spreadsheet.h
+++ Spreadsheet.h
@@ -5,9 +5,25 @@
 #ifndef SIMPLE_SPREADSHEET_SPREADSHEET_H
 #define SIMPLE_SPREADSHEET_SPREADSHEET_H
 
+#include <wx/frame.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
+#include <wx/sizer.h>
+#include <vector>
 
-class Spreadsheet {
-
+class Spreadsheet: public wxFrame {
+public:
+    explicit Spreadsheet(int numOfColumns, wxWindow *parent = nullptr, wxWindowID id = wxID_ANY,
+                         const wxString &title = wxEmptyString, const wxPoint &pos = wxDefaultPosition,
+                         const wxSize &size = wxDefaultSize,
+                         long style = wxDEFAULT_FRAME_STYLE, const wxString &name = wxFrameNameStr);
+private:
+    int columns;
+    const int rows = 4;
+    std::vector<wxTextCtrl *> cells;
+    std::vector<wxTextCtrl *> results;
+    wxGridSizer *gridSizerCells;
+    wxGridSizer *gridSizerResults;
 };
 
 
