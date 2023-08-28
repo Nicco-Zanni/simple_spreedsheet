660ec8435210125153ce3e971da1ae75139a9d23
nicco <niccozanni@gmail.com>
2023-08-28 18:28:20 +0200

    A bug was corrected in the update method in SumCalculator.cpp

--- SumCalculator.cpp
+++ SumCalculator.cpp
@@ -6,10 +6,8 @@
 
 void SumCalculator::update() {
     sum = 0;
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < column; j++){
-            sum += spreadsheet->getCellValueAt(i, j);
-        }
+    for(int i = 0; i < column; i++){
+            sum += spreadsheet->getCellValueAt(row, i);
     }
     spreadsheet->setResult(sum, row, column);
 }
