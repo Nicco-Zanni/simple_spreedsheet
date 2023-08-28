109eb3a7bbdc3839d65ab173704de92fd4b513e9
nicco <niccozanni@gmail.com>
2023-08-28 16:54:02 +0200

    The constructor and the setupGrid method were implemented

--- Spreadsheet.cpp
+++ Spreadsheet.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Spreadsheet.h"
+#include <wx/wx.h>
+
+Spreadsheet::Spreadsheet(int numOfColumns, wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &pos,
+                         const wxSize &size, long style, const wxString &name) : wxFrame(parent, id, title, pos, size,
+                                                                                         style, name), gridSizerCells(nullptr){
+    if(numOfColumns < 2)
+        columns = 1;
+    else
+        columns = numOfColumns;
+    wxFont font(14, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
+    this->SetFont(font);
+
+    setupGrid();
+}
+
+void Spreadsheet::setupGrid() {
+    auto sizer = new wxBoxSizer(wxVERTICAL);
+    auto panel = new wxScrolled<wxPanel>(this, wxID_ANY);
+    auto panelSizer = new wxBoxSizer(wxVERTICAL);
+
+    gridSizerCells = new wxGridSizer(rows, columns, 0, 0);
+    panel->SetScrollRate(5, 0);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns - 1; j++) {
+            auto cell = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize,
+                                       wxTE_PROCESS_ENTER | wxTE_CENTRE, wxTextValidator(wxFILTER_NUMERIC));
+            gridSizerCells->Add(cell, 1, wxALL | wxEXPAND, 5);
+            cells.push_back(cell);
+        }
+        auto result = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize,
+                                     wxTE_READONLY | wxTE_CENTRE);
+        gridSizerCells->Add(result, 1, wxALL | wxEXPAND, 5);
+        cells.push_back(result);
+    }
+
+    auto top_panel = new wxPanel(this, wxID_ANY);
+    auto bottom_panel = new wxPanel(this, wxID_ANY);
+
+    panelSizer->Add(top_panel, 1, wxALL | wxEXPAND, 5);
+    panelSizer->Add(gridSizerCells, 0, wxALL | wxEXPAND, 5);
+    panelSizer->Add(bottom_panel, 1, wxALL | wxEXPAND, 5);
+    panel->SetSizerAndFit(panelSizer);
+    sizer->Add(panel, 1, wxALL | wxEXPAND, 5);
+    SetSizerAndFit(sizer);
+}
