08f870606fa84d4544c0dd26d0b03a50f950dcf2
nicco <niccozanni@gmail.com>
2023-08-28 18:18:23 +0200

    update method was implemented in SumCalculator.cpp

--- SumCalculator.cpp
+++ SumCalculator.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "SumCalculator.h"
+
+void SumCalculator::update() {
+    sum = 0;
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < column; j++){
+            sum += spreadsheet->getCellValueAt(i, j);
+        }
+    }
+    spreadsheet->setResult(sum, row, column);
+}
