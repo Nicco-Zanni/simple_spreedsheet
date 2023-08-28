e617ab65a2969df702ea0c0f2541366d679f1ba5
nicco <niccozanni@gmail.com>
2023-08-28 17:20:52 +0200

    The Subject class was implemented

---
+++ Subject.h
@@ -0,0 +1,22 @@
+//
+// Created by nicco on 28/08/23.
+//
+
+#ifndef SIMPLE_SPREADSHEET_SUBJECT_H
+#define SIMPLE_SPREADSHEET_SUBJECT_H
+#include "Observer.h"
+
+class Subject {
+public:
+    virtual ~Subject() {}
+
+    virtual void subscribe(Observer *obs) = 0;
+
+    virtual void unsubscribe(Observer *obs) = 0;
+
+    virtual void notify() = 0;
+
+};
+
+
+#endif //SIMPLE_SPREADSHEET_SUBJECT_H
