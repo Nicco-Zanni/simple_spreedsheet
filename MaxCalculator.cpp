f0f17a1794df28462c29656c8dd85d57316716b1
nicco <niccozanni@gmail.com>
2023-08-29 17:03:51 +0200

    corrected a bug that allowed to use the values of other row if the max row is empty

--- MaxCalculator.cpp
+++ MaxCalculator.cpp
@@ -6,11 +6,11 @@
 
 void MaxCalculator::update() {
     int i = 0;
-    while(spreadsheet->isEmpty(row, i)) {
+    while(i < column && spreadsheet->isEmpty(row, i)) {
         i++;
     }
     max = spreadsheet->getCellValueAt(row, i);
-    for(int j = 1; j < column; j++){
+    for(int j = i; j < column; j++){
         if(max < spreadsheet->getCellValueAt(row, j) && !spreadsheet->isEmpty(row, j)){
             max = spreadsheet->getCellValueAt(row, j);
         }
