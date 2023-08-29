04104661a9d8aa0328ed96442a012de1ac02be43
nicco <niccozanni@gmail.com>
2023-08-29 16:02:05 +0200

    Update method was implemented in MaxCalculator.cpp

--- MaxCalculator.cpp
+++ MaxCalculator.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "MaxCalculator.h"
+
+void MaxCalculator::update() {
+    max = spreadsheet->getCellValueAt(row, 0);
+    for(int i = 1; i < column; i++){
+        if(max < spreadsheet->getCellValueAt(row, i)){
+            max = spreadsheet->getCellValueAt(row, i);
+        }
+    }
+    spreadsheet->setResult(max, row, column);
+}
