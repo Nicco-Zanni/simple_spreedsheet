6db55f77da1d547346fd20bd648744837afc198d
nicco <niccozanni@gmail.com>
2023-09-01 19:15:13 +0200

    Added Cell class, his constructor and his destructor

---
+++ Cell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nicco on 01/09/23.
+//
+
+#include "Cell.h"
---
+++ Cell.h
@@ -0,0 +1,36 @@
+//
+// Created by nicco on 01/09/23.
+//
+
+#ifndef SIMPLE_SPREADSHEET_CELL_H
+#define SIMPLE_SPREADSHEET_CELL_H
+#include "Observer.h"
+#include "Subject.h"
+#include <wx/wx.h>
+#include <list>
+#include <string>
+
+class Cell: public Observer, public Subject{
+public:
+    Cell(int row, int column, wxTextCtrl* Textctrl): row(row), column(column), value(0), formula("none"), textCtrl(Textctrl){
+        textCtrl->Bind(wxEVT_TEXT, &Cell::notify, this);
+    }
+    ~Cell(){
+        observers.clear();
+        subjects.clear();
+        textCtrl->Unbind(wxEVT_TEXT, &Cell::notify, this);
+    }
+
+private:
+    int row, column;
+    double value;
+    std::string formula;
+    wxTextCtrl* textCtrl;
+    std::list<Observer*> observers;
+    std::list<Cell*> subjects;
+
+
+};
+
+
+#endif //SIMPLE_SPREADSHEET_CELL_H
