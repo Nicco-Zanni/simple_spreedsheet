6efd8a034da4e44b38ee56a6b13d5e0a2843636f
nicco <niccozanni@gmail.com>
2023-09-07 16:53:03 +0200

    testSpreadsheetConstructor was added

--- Google_tests/TestSpreasheet.cpp
+++ Google_tests/TestSpreasheet.cpp
@@ -4,12 +4,13 @@
 #include <gtest/gtest.h>
 #include "../Spreadsheet.h"
 
-TEST(SpreadsheetSuite, testSetGridSize){
-    auto ptr = dynamic_cast<Spreadsheet *> (wxTheApp->GetTopWindow());
-    ptr->setGridSize(-1, 2);
+TEST(SpreadsheetSuite, testSpreadsheetConstructor){
+    auto ptr = new Spreadsheet(-1, 2,NULL, wxID_ANY, "Spreadsheet");
     EXPECT_EQ(ptr->getRows(), 1);
     EXPECT_EQ(ptr->getColumns(), 2);
-    ptr->setGridSize(1, -5);
-    EXPECT_EQ(ptr->getRows(), 1);
-    EXPECT_EQ(ptr->getColumns(), 2);
+    EXPECT_EQ(ptr->getCells().size(), 2);
+    auto ptr2 = new Spreadsheet(1, -4,NULL, wxID_ANY, "Spreadsheet");
+    EXPECT_EQ(ptr2->getRows(), 1);
+    EXPECT_EQ(ptr2->getColumns(), 2);
+    EXPECT_EQ(ptr2->getCells().size(), 2);
 }
