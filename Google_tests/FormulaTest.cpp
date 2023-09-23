567c69caf3aaa0103a33efc0d7c37e3c5cd80dd0
nicco <niccozanni@gmail.com>
2023-09-23 16:59:10 +0200

    MinFormulaComputeTest was added

--- Google_tests/FormulaTest.cpp
+++ Google_tests/FormulaTest.cpp
@@ -17,8 +17,16 @@ TEST(FormulaSuite, SumFormulaComputeTest){
 
 TEST(FormulaSuite, MaxFormulaComputeTest){
     MaxFormula formula;
-    std::vector<double> values = {1.333, 2.457, -3.9863, 4.456, 5.1};
+    std::vector<double> values = {1.333, 2.457, -3.9863, 4.456, 5.1, 0};
     EXPECT_EQ(formula.compute(values), 5.1);
     values = {-1.333, -2.457, -3.9863, -4.456, -5.1, -1.334};
     EXPECT_EQ(formula.compute(values), -1.333);
+}
+
+TEST(FormulaSuite, MinFormulaComputeTest){
+    MinFormula formula;
+    std::vector<double> values = {1.333, 2.457, -3.9863, 4.456, 5.1, 0};
+    EXPECT_EQ(formula.compute(values), -3.9863);
+    values = {-1.333, -2.457, -3.9863, -4.456, -5.1896, -1.334, -5.1895};
+    EXPECT_EQ(formula.compute(values), -5.1896);
 }
