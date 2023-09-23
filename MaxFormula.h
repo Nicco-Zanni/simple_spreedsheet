1c3b381d536bec03b0bb6f3d3a3f0fa946c46442
nicco <niccozanni@gmail.com>
2023-09-23 11:53:17 +0200

    MaxFormula was added

---
+++ MaxFormula.h
@@ -0,0 +1,14 @@
+//
+// Created by nicco on 23/09/23.
+//
+
+#ifndef SIMPLE_SPREADSHEET_MAXFORMULA_H
+#define SIMPLE_SPREADSHEET_MAXFORMULA_H
+
+
+class MaxFormula {
+
+};
+
+
+#endif //SIMPLE_SPREADSHEET_MAXFORMULA_H
