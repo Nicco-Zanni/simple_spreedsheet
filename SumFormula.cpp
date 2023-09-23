b2b709b17e2afdf3db99c83fc00d065a36c8f2c6
nicco <niccozanni@gmail.com>
2023-09-23 11:41:52 +0200

    SumFormula class was added

---
+++ SumFormula.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nicco on 23/09/23.
+//
+
+#include "SumFormula.h"
---
+++ SumFormula.h
@@ -0,0 +1,14 @@
+//
+// Created by nicco on 23/09/23.
+//
+
+#ifndef SIMPLE_SPREADSHEET_SUMFORMULA_H
+#define SIMPLE_SPREADSHEET_SUMFORMULA_H
+
+
+class SumFormula {
+
+};
+
+
+#endif //SIMPLE_SPREADSHEET_SUMFORMULA_H
