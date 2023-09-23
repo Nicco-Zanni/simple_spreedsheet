11f27c159c71ff3253e27d9de9d95dcaf0c3fb55
nicco <niccozanni@gmail.com>
2023-09-23 16:56:23 +0200

    SumFormulaComputeTest was added

---
+++ Google_tests/FormulaTest.cpp
@@ -0,0 +1,24 @@
+//
+// Created by nicco on 23/09/23.
+//
+#include "../SumFormula.h"
+#include "../MaxFormula.h"
+#include "../MinFormula.h"
+#include "../MeanFormula.h"
+#include <gtest/gtest.h>
+
+TEST(FormulaSuite, SumFormulaComputeTest){
+    SumFormula formula;
+    std::vector<double> values = {1.333, 2.457, -3.9863, 4.456, 5.1};
+    EXPECT_EQ(formula.compute(values), 9.3597);
+    values = {-1.333, -2.457, -3.9863, -4.456, -5.1, 0};
+    EXPECT_EQ(formula.compute(values), -17.3323);
+}
+
+TEST(FormulaSuite, MaxFormulaComputeTest){
+    MaxFormula formula;
+    std::vector<double> values = {1.333, 2.457, -3.9863, 4.456, 5.1};
+    EXPECT_EQ(formula.compute(values), 5.1);
+    values = {-1.333, -2.457, -3.9863, -4.456, -5.1, 0};
+    EXPECT_EQ(formula.compute(values), 0);
+}
